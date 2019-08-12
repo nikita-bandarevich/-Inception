fca659aa069397ff0d0f7c7504b1902901f2aa38
Matthis Parisot <matthis.parisot99@gmail.com>
2019-08-12 18:20:11 +0200

    adding routes for neurolink

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'neurolinks/new'
-  get 'neurolinks/create'
+  # get 'neurolinks/new'
+  # get 'neurolinks/create'
   # get 'dreams/index'
   # get 'dreams/new'
   # get 'dreams/create'
 
-  resources :dreams
+  resources :dreams do
+    resources :neurolinks, only: [:new, :create]
+  end
+
   devise_for :escapists
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
