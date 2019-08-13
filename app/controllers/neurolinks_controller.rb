d064af8096d9b7306c7d0664d02d5d13849d5364
Nikita Bandarevich <127barcelona@gmail.com>
2019-08-13 11:59:46 +0200

    added neurolinks controller methods, the view displays the correct route and message, let's start testing all the front-end with variables

--- app/controllers/neurolinks_controller.rb
+++ app/controllers/neurolinks_controller.rb
@@ -1,7 +1,23 @@
 class NeurolinksController < ApplicationController
   def new
+    @dream = Dream.find(params[:dream_id])
+    @neurolink = Neurolink.new
   end
 
   def create
+    @neurolink = Neurolink.new(neurolinks_strong_parameters)
+    @neurolink.dream = Dream.find(params[:dream_id])
+
+    if @neurolink.save
+      redirect_to dream_path(@neurolink.dream)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def neurolinks_strong_parameters
+    params.require(:neurolink).permit(:start_date, :end_date, :escapist_id)
   end
 end
