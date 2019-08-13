901b81451ee5be182558463c41341d49142fac1a
Nikita Bandarevich <127barcelona@gmail.com>
2019-08-13 11:42:36 +0200

    updated the dream controller, it is fully functional with 7 methods now, plus added all the necessary view for it

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,10 +1,11 @@
 class DreamsController < ApplicationController
+  before_action :set_dream, only: [:show, :edit, :update, :destroy]
+
   def index
     @dreams = Dream.all
   end
 
   def show
-    @dream = Dream.find(params[:id])
   end
 
   def new
@@ -21,7 +22,27 @@ class DreamsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @dream.update(dream_strong_params)
+      redirect_to @dream, notice: 'dream is revised, continue to the matrix'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dream.destroy
+    redirect_to dreams_path, notice: "dream is out of the matrix"
+  end
+
   private
+
+  def set_dream
+    @dream = Dream.find(params[:id])
+  end
 
   def dream_strong_params
     params.require(:dream).permit(:title, :price, :category, :description)
