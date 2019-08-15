3c5970b4694aced261e23dd0c70fbce82f9913e4
Matthis Parisot <matthis.parisot99@gmail.com>
2019-08-15 13:22:37 +0200

    categoory

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -4,6 +4,6 @@ class Dream < ApplicationRecord
   has_many :escapists, through: :neurolinks
 
   validates :title, uniqueness: true, presence: true, length: { maximum: 13 }
-  validates :category, presence: true, inclusion: { in: ["Drama", "Comedy", "Thriller", "Action", "Fantastic", "Horror", "Science Fiction", "Polar", "Period film", "Animated film", "Storytelling", "Western", "Parody", "Musical comedy", "Documentary"] }
+  validates :category, presence: true, inclusion: { in: ["Daydream", "Lucid Dream", "Nightmare", "Prophetic Dream", "Epic Dream", "False Awakenings?", "Sexual Dream", "Deja vu Dream", "Clairvoyant Dream"] }
   validates :price, presence: true
 end
