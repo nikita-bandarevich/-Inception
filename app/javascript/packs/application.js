89f7cb6da2331c8f053887f4dffef996ddf6aa3f
Nikita Bandarevich <127barcelona@gmail.com>
2019-08-16 00:49:39 +0200

    both carousel and animation of sliding letters are working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,8 @@ import "bootstrap";
 import "tiny-slider/dist/tiny-slider.css";
 import { initSlider } from '../plugins/tns';
 import { initTyper } from '../components/_typing_animation';
+import { listener } from '../components/_animation_direction';
 
 initSlider();
 initTyper();
-
+listener();
