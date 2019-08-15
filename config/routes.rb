198b95520852cb91a3761d08ee939a75aea4ec4e
Matthis Parisot <matthis.parisot99@gmail.com>
2019-08-15 13:03:41 +0200

    inclusion edit create page and validate

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   # get 'dreams/new'
   # get 'dreams/create'
 
+
   resources :dreams do
     resources :neurolinks, only: [:new, :create]
   end
 
-  devise_for :escapists
+  devise_for :escapists, controllers: { registrations: "escapists/registrations" }, controllers: { sessions: "escapists/sessions" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
