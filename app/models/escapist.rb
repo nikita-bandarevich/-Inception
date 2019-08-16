49e87d4cbddbabb9c3f319c9d127ad80e634eb3b
Jaime Cruz <jaimecruza@me.com>
2019-08-16 13:27:42 +0200

    tryng to push to github

--- app/models/escapist.rb
+++ app/models/escapist.rb
@@ -1,6 +1,7 @@
 class Escapist < ApplicationRecord
   has_many :dreams
   has_many :neurolinks
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
